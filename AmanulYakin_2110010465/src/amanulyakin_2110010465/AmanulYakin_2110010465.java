bc7bfc9324ad31ddad645325415982b104b97b0c
yakinnn <133826317+yakinnn@users.noreply.github.com>
2023-06-14 09:50:59 +0800

    Add files via upload

---
+++ AmanulYakin_2110010465/src/amanulyakin_2110010465/AmanulYakin_2110010465.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package amanulyakin_2110010465;
+import akademik.*;
+import forms.frameAkademik;
+
+/**
+ *
+ * @author ASUS
+ */
+public class AmanulYakin_2110010465 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //objek class dataguru
+        //dataguru guru = new dataguru("Sri", "Banjarmasin", "20 Mei 1990", "Islam", "Perempuan", "0898789", "sriasri@gmail.com", "Jalan Sultan Adam", "S2", "Menikah", "Guru Matematika");
+        //Output
+        /*System.out.println("Nama guru : " +guru.getNamaGuru());
+        System.out.println("Tempat lahir guru : " +guru.getTempatLahir());
+        System.out.println("Tanggal lahir guru : " +guru.getTanggalLahir());
+        System.out.println("Agama guru : " +guru.getAgama());
+        System.out.println("Jenis kelamin guru : " +guru.getJenisKelamin());
+        System.out.println("Telpon guru : " +guru.getTelpon());
+        System.out.println("Email guru : " +guru.getEmail());
+        System.out.println("Alamat guru : " +guru.getAlamat());
+        System.out.println("Pendidikan guru : " +guru.getPendidikan());
+        System.out.println("Status guru : " +guru.getStatus());
+        System.out.println("Jabatan guru : " +guru.getJabatan());
+        
+        //objek class datasiswa
+        datasiswa Siswa = new datasiswa(1234, "Yakin", "Banjarmasin", "29 Oktober 2002", "Islam", "Laki-laki", "Jalan Sungai Jingah", "085155395675", "SMKN 2 Banjarmasin");
+        //Output
+        System.out.println("NIS siswa : " +Siswa.getNIS());
+        System.out.println("Nama siswa : " +Siswa.getNamaSiswa());
+        System.out.println("Tempat lahir siswa : " +Siswa.getTempatLahir());
+        System.out.println("Tanggal lahir siswa : " +Siswa.getTanggalLahir());
+        System.out.println("Agama siswa : " +Siswa.getAgama());
+        System.out.println("Jenis kelamin siswa : " +Siswa.getJenisKelamin());
+        System.out.println("Alamat siswa : " +Siswa.getAlamat());
+        System.out.println("Telpon siswa : " +Siswa.getTelpon());
+        System.out.println("Asal sekolah siswa : " +Siswa.getAsalSekolah());
+         
+        //objek class jadwalsiswa
+        jadwalsiswa jadwal = new jadwalsiswa("07:30", "Senin");
+        //Output
+        System.out.println("Jam masuk : " +jadwal.getJamMasuk());
+        System.out.println("Hari masuk : " +jadwal.getHariMasuk());
+        
+        //objek class kelas
+        kelas Kelas = new kelas(12, 29);
+        //Output
+        System.out.println("Kelas : " +Kelas.getKelas());
+        System.out.println("Kapasitas kelas : " +Kelas.getKapasitas());
+        
+        //objek kelas pelajaran
+        pelajaran Pelajaran = new pelajaran("Matematika", 75);
+        //Output
+        System.out.println("Mata pelajaran : " +Pelajaran.getNamaPelajaran());
+        System.out.println("KKM mapel : " +Pelajaran.getKKM());
+        
+        //objek class nilai
+        nilai Nilai = new nilai(80, 85, 80, 90);
+        //Output
+        System.out.println("Nilai UTS : " +Nilai.getNilaiUts());
+        System.out.println("Nilai UAS : " +Nilai.getNilaiUas());
+        System.out.println("Nilai Tugas : " +Nilai.getNilaiTugas());
+        System.out.println("Nilai Absen : " +Nilai.getNilaiAbsen());*/
+    
+    frameAkademik myFrame = new frameAkademik();//membuat objek myFrame
+    
+    myFrame.setVisible(true);
+    
+    }
+    
+}
